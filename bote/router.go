6bdc66b1648d359fe94cf8ef032bd9d8f62e163e
Jeff Becker <ampernand@gmail.com>
2017-01-26 11:52:14 -0500

    more

--- bote/router.go
+++ bote/router.go
@@ -17,5 +17,13 @@ func NewRouter(cfg config.RouterConfig) *Router {
 func (r *Router) InjectNetwork(s i2p.PacketSession) {
 	log.Infof("Aquired net context with address %s", s.I2PAddr().Base32())
 	r.session = s
+}
 
+// close router and network context
+func (r *Router) Close() {
+	log.Info("closing i2pbote context...")
+	if r.session != nil {
+		log.Debug("closing network session")
+		r.session.Close()
+	}
 }
